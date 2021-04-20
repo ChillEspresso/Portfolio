5acbdd6f91637a72c74355202c0c22e93c4121be
Dennis Bagot <dennisbagot1998@gmail.com>
2021-04-20 10:37:50 -0400

    add close animation

--- js/index.js
+++ js/index.js
@@ -1,5 +1,16 @@
 const navToggle = document.querySelector('.nav-toggle');
+const navLinks = document.querySelectorAll('.nav__link');
 
+
+//Opens the Nav Menu
 navToggle.addEventListener('click', () => {
     document.body.classList.toggle('nav-open');
 });
+
+
+//When an option on the nav menu is selected the nav menu will be closed
+navLinks.forEach(link => {
+    link.addEventListener('click', () => {
+        document.body.classList.remove('nav-open');
+    })
+})
